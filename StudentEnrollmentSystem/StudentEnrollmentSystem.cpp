38f7bba2432ca5097c17c4e5c75ebd73c8460497
Mxolisi Sigasa <mxolisi.sigasa@fdmgroup.com>
2023-04-18 16:31:09 +0200

    Added details formatter

--- StudentEnrollmentSystem/StudentEnrollmentSystem.cpp
+++ StudentEnrollmentSystem/StudentEnrollmentSystem.cpp
@@ -4,32 +4,116 @@
 #include <iostream>
 #include <conio.h>
 #include <fstream>
+#include <string>
+
 #include "student.h"
+#include "module.h"
 using namespace std;
 
-int main()
-{
-    list<student> registered_students;
-
-    cout << "Hello World!\n";
-
-    _getch();
-}
 
 void write_to_file(list<student> students)
 {
-    ofstream student_file("student_grades.csv");
+	ofstream student_file("student_grades.csv");
 
-    for (auto student : students)
-    {
-        string student_details = "Hello. My name is {name}. I'm {age}.";
-    }
+	student_file << "STUDENT NUMBER, INITIALS, SURNAME, SEX, AGE, ELD 211, EPE 221, AEC 201, AGE 213\n";
 
+	for (auto student : students)
+	{
+		student_file << student.formatted_details+"\n";
+	}
 
-
-
-    student_file.close();
+	student_file.close();
 }
+
+student enroll_student()
+{
+	long student_number;
+	string initials;
+	string surname;
+	string sex;
+	int age;
+
+	cout << " Enter student number : ";
+	cin >> student_number;
+	cout << " Enter student initials: ";
+	cin >> initials;
+	cout << " Enter student surname : ";
+	cin >> surname;
+	cout << " Enter student sex : ";
+	cin >> sex;
+	cout << " Enter student age : ";
+	cin >> age;
+
+	int eld_marks;
+	cout << "Enter student marks for this module ( ELD 211 ) :";
+	cin >> eld_marks;
+	module module1 = module("ELD 211", eld_marks);
+
+	int epe_marks;
+	cout << "Enter student marks for this module ( EPE 221 ) :";
+	cin >> epe_marks;
+	module module2 = module("EPE 221", epe_marks);
+
+	int aec_marks;
+	cout << "Enter student marks for this module ( AEC 201 ) :";
+	cin >> aec_marks;
+	module module3 = module("AEC 201", aec_marks);
+
+	int age_marks;
+	cout << "Enter student marks for this module ( AGE 213 ) :";
+	cin >> age_marks;
+	module module4 = module("AGE 213", age_marks);
+
+	list<module> modules = { module1,module2,module3,module4 };
+
+	string formatted_student_details = to_string(student_number) + "," + initials + "," + surname + "," + sex + "," +
+		to_string(age) + "," + to_string(eld_marks) + "," + to_string(epe_marks) + "," + to_string(aec_marks) + "," + to_string(age_marks);
+
+	return student(student_number, initials, surname, sex, age, modules, formatted_student_details);
+}
+
+void get_name(list<string> strs)
+{
+	for (auto str : strs)
+	{
+		cout << str << endl;
+	}
+}
+
+int main()
+{
+	list<student> registered_students;
+	bool stop_app = false;
+	do
+	{
+		get_name({"Inno", "Mxo","Thomas"});
+		cout << "Please select an option below:"<<endl;
+		cout << "1. Enroll a student " << endl;
+		cout << "2. Write student data into a file " << endl;
+		cout << "0. Exit the program!!!" << endl;
+		int option = 0;
+		cin >> option;
+
+		if (option == 0) {
+			stop_app = true;
+			cout << "Exiting the program. Goodbye !!";
+		}
+		else if (option == 1) {
+			student enrolled_student = enroll_student();
+			registered_students.push_back(enrolled_student);
+		}
+		else if (option == 2) {
+			write_to_file(registered_students);
+		}
+		else
+		{
+			cout << "Wrong option !! try again";
+		}
+	} while (!stop_app);
+
+	_getch();
+}
+
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
--- StudentEnrollmentSystem/student.h
+++ StudentEnrollmentSystem/student.h
@@ -1,48 +1,31 @@
 #pragma once
 #include <iostream>
 #include <list>
-
+#include <string>
 #include "Module.h"
 
-using namespace std;
+using namespace std;  // NOLINT(clang-diagnostic-header-hygiene)
+
 class student
 {
 public:
 	long student_number;
 	string initials;
 	string surname;
-	char sex;
+	string sex;
 	int age;
 	list<module> modules;
+	string formatted_details;
 
-	student(long student_number, string initials, string surname, char sex, int age)
+	student(long student_number, string initials, string surname, string sex, int age, list<module> modules, string formatted_details)
 	{
 		this->student_number = student_number;
 		this->initials = initials;
 		this->surname = surname;
 		this->sex = sex;
 		this->age = age;
+		this->modules = modules;
+		this->formatted_details = formatted_details;
 	}
-
-	list<module> initialize_modules()
-	{
-		list<string> module_names;
-		module_names.emplace_back("ELD 211");
-		module_names.emplace_back("EPE 221");
-		module_names.emplace_back("AEC 201");
-		module_names.emplace_back("AGE 213");
-
-		list<module> student_modules;
-
-		for (const auto& module_name : module_names)
-		{
-			module student_module = module(module_name, 0);
-
-			student_modules.emplace_back(student_module);
-		}
-
-		return student_modules;
-	}
-
 };
 
